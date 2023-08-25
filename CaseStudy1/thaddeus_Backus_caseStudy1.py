923cd5ff2d668275aaddf84e49bf31c5c9fe5c59
taddbackus <104407802+taddbackus@users.noreply.github.com>
2023-08-25 08:52:15 -0400

    added in some L1 work

--- CaseStudy1/thaddeus_Backus_caseStudy1.py
+++ CaseStudy1/thaddeus_Backus_caseStudy1.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_predict, cross_val_score
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
 
 scData = pd.read_csv('/Users/taddbackus/School/fall23/qtw/cs1/data/train.csv')
 
@@ -25,3 +29,21 @@ plt.scatter(y,yPred)
 #plt.show()
 print(yPred.shape)
 
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_scaled = pd.DataFrame(data=X_scaled,columns=X.columns)
+print(X_scaled)
+
+lrS = LinearRegression()
+lrS.fit(X_scaled,y)
+for i in range(len(X_scaled.columns)):
+    print(X_scaled.columns[i],lrS.coef_[i])
+
+lrL1 = Lasso(alpha=1)
+lrL1.fit(X_scaled,y)
+
+cvLassoModel = Lasso()
+for i in np.arange(0.0,1.0,.01):
+    cvLassoModel.alpha = i
+    print(i, cross_val_score(cvLassoModel,X_scaled,y,scoring='neg_mean_squared_error'))
+    print('==========================')
